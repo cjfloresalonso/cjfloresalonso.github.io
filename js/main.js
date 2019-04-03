0339d167a5614b81379ced8b2fef9f39aea63f95
Christopher Flores Alonso <cjfloresalonso@gmail.com>
2019-04-04 07:47:06 +1000

    Added some quotes.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 const quotes = [
   ["Learn to love problem solving; life is a just series just a series problems to solve.", "Mercedes Alonso"],
+  ["Learn to love problem solving; life is a just series just a series problems to solve.", "Mercedes Alonso"],
+  ["Learn to love problem solving; life is a just series just a series problems to solve.", "Mercedes Alonso"],
+  ["Que todos los males fueran como este...", "Mercedes Alonso"],
+  ["If only all bad things where like this one...", "Mercedes Alonso"],
   ["Testimonials are a cringy marketing tactic.", "cjfloresalonso"],
   ["The only true wisdom is in knowing you know nothing.", "Socrates"],
   ["It is during our darkest moments that we must focus to see the light.", "Aristotle"],
   ["Be yourself; everyone else is already taken.", "Oscar Wilde"],
   ["In three words I can sum up everything I've learned about life: it goes on.", "Robert Frost"],
-  ["If you tell the truth, you don't have to remember anything.", "Mark Twain"]
+  ["If you tell the truth, you don't have to remember anything.", "Mark Twain"],
+  ["oof.", "discoverer of CVE-2008-0166"],
+  ["Try Harder!", "Offensive Security"]
   
 ]
 
